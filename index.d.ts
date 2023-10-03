157edd85b1fe74a0698fa37f544971a9fb955682
Winston Hoy <winston@commonenergy.us>
2023-10-03 12:37:24 -0400

    improve type defs

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,12 @@
-export function listener(message: any, topic: string): void;
+export type listener = (message: any, topic: string) => void;
 export type token = string;
 export type topic = string;
 
-export namespace Pubsub {
-  export function once (topic: topic, listener: listener): token;
-  export function pub (topic: topic, message: any): Promise<boolean>;
-  export function sub (topic: topic, listener: listener): token;
-  export function unsub (topic: topic | token | listener, listener?: listener): number;
+export declare class Pubsub {
+  once (topic: topic, listener: listener): token;
+  pub (topic: topic, message: any): Promise<boolean>;
+  sub (topic: topic, listener: listener): token;
+  unsub (topic: topic | token | listener, listener?: listener): number;
 }
 
 export const pubsub: Pubsub;
